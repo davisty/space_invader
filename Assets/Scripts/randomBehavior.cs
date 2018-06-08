dc38981d4f4b9e5b525fc2e17c46412e21bb86cf
Tyler Davis <tylerdavis@mail.usf.edu>
2018-06-08 15:30:55 -0400

    06/08/18 Update
    Modified player ship sprite.
    Modified enemy ship sprite.
    Modified player projectile.
    Modified enemy projectile.
    Modified start menu.
    Created start menu animation.
    Edited some fonts.

---
+++ Assets/Scripts/randomBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomBehavior : MonoBehaviour {
+	public GameObject projectile;
+	public float projectileSpeed = 3f;
+	public float shotsPerSecond = .9f;
+	
+	public AudioClip fireSound;
+	public AudioClip deathSound;
+	
+	private ScoreKeeper scoreKeeper;
+
+
+	void Update(){
+		float prob = shotsPerSecond * Time.deltaTime;
+		if(Random.value < prob){
+			Fire ();
+		}
+	}
+	
+	void Fire(){
+		GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0,+projectileSpeed);
+		AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
+	
+}
